92fc3f6868b0362b99538ca6b73bfa2d9cce316b
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-02 10:10:18 +0430

    update: example

--- example/main.go
+++ example/main.go
@@ -7,9 +7,36 @@ import (
 )
 
 func main() {
-	openports := scanner.Scan("", 2048)
+	{
+		// scanning ports 0 to 2048
+		openPorts := scanner.Scan("", []int{})
 
-	for _, port := range openports {
-		fmt.Printf("%d open\n", port)
+		for _, port := range openPorts {
+			fmt.Printf("%d open\n", port)
+		}
+	}
+	{
+		// scanning ports 100 to 5000
+		openPorts := scanner.Scan("", []int{100, 5000})
+
+		for _, port := range openPorts {
+			fmt.Printf("%d open\n", port)
+		}
+	}
+	{
+		// scanning ports 0 to 5000
+		openPorts := scanner.Scan("", []int{5000})
+
+		for _, port := range openPorts {
+			fmt.Printf("%d open\n", port)
+		}
+	}
+	{
+		// scanning ports 0 to 5000 with 45 workers
+		openPorts := scanner.Scan("", []int{5000}, 45)
+
+		for _, port := range openPorts {
+			fmt.Printf("%d open\n", port)
+		}
 	}
 }
