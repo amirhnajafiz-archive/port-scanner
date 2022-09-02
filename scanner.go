14634a4468c8693261b6c83bdc361253407b084d
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-02 10:06:45 +0430

    add: port range as array

--- scanner.go
+++ scanner.go
@@ -6,14 +6,24 @@ import (
 	"github.com/amirhnajafiz/port-scanner/worker"
 )
 
+const offset = 2048
+
 // Scan takes an uri, portRange and number of workers and will
 // test the uri ports in portRange (0, portRange) with workers and then
 // returns the open ports.
-func Scan(uri string, portRange int, workers ...int) []int {
+func Scan(uri string, portRange []int, workers ...int) []int {
 	numberOfWorkers := 100
 	if len(workers) > 0 {
 		numberOfWorkers = workers[0]
 	}
+
+	if len(portRange) == 0 {
+		portRange = append(portRange, 0, offset)
+	} else if len(portRange) == 1 {
+		portRange = append(portRange, offset)
+	}
+
+	sort.Ints(portRange)
 
 	ports := make(chan int, numberOfWorkers)
 	response := make(chan int)
@@ -25,12 +35,12 @@ func Scan(uri string, portRange int, workers ...int) []int {
 	}
 
 	go func() {
-		for i := 0; i <= portRange; i++ {
+		for i := portRange[0]; i <= portRange[1]; i++ {
 			ports <- i
 		}
 	}()
 
-	for i := 0; i <= portRange; i++ {
+	for i := portRange[0]; i <= portRange[1]; i++ {
 		port := <-response
 		if port != 0 {
 			openPorts = append(openPorts, port)
